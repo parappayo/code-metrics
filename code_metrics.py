9003a4d7164690acb99eae43302069b045e1a26a
Jason Estey <esteyj@yahoo.com>
2018-06-19 16:05:11 -0700

    expanding on the command line options

--- code_metrics.py
+++ code_metrics.py
@@ -2,17 +2,27 @@
 Generates metrics for given source code input.
 """
 
-import sys, re, argparse
-
-target_lang = 'python'
+import sys, os, re, argparse
 
 python_func_regex = re.compile('def\s+(.*)\(.*:')
+
+lang_file_ext = {
+	'.py': 'python',
+	'.js': 'javascript',
+	'.cs': 'csharp',
+	'.c': 'c',
+	'.cpp': 'cplusplus',
+}
 
 def distribution(values):
 	result = {}
 	for value in values:
 		result[value] = result.get(value, 0) + 1
 	return result
+
+def file_ext_lang(path):
+	filename, file_ext = os.path.splitext(path)
+	return lang_file_ext.get(file_ext, 'python')
 
 def ends_with_whitespace(line):
 	if len(line) < 1: return False
@@ -68,7 +78,10 @@ def report_functions(lines):
 		result[name] = report_function(lines)
 	return result
 
-def report(code):
+def report(code, target_lang):
+	if target_lang != 'python':
+		print('input lang not supported yet:', target_lang)
+		exit()
 	lines = code.splitlines()
 	return {
 		'line_count': len(lines),
@@ -78,16 +91,37 @@ def report(code):
 		'functions': report_functions(lines)
 	}
 
+def print_report(report, format):
+	if format == 'pydict':
+		print(report)
+	else:
+		print('html output not supported yet')
+
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument('input_file_path', metavar='file')
-	parser.add_argument('--lang', nargs='?', help='force programming language (python)')
+	parser.add_argument('--lang', nargs='?',
+		help='override input language',
+		choices=['c', 'cplusplus', 'javascript', 'python'])
+	parser.add_argument('--format', nargs='?',
+		help='override output format',
+		choices=['html', 'pydict'])
 	args = parser.parse_args()
 
-	if args.lang:
-		target_lang = args.lang
-
 	path = args.input_file_path
+
+	target_lang = args.lang
+	if not target_lang:
+		ext_lang = file_ext_lang(path)
+		if ext_lang:
+			target_lang = ext_lang
+		else:
+			target_lang = 'python'
+
+	output_format = args.format
+	if not output_format:
+		output_format = 'html'
+
 	with open(path, "r") as input_file:
 		code = input_file.read()
-		print(report(code))
+		print_report(report(code, target_lang), args.format)
